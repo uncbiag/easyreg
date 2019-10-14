b7868b8fd454bebd87612f8f581999b0e333f247
hbgtjxzbbx <zyshen021@gmail.com>
2019-10-14 11:14:04 -0400

    fix spacing in saving orginal sz map

--- model_pool/base_mermaid.py
+++ model_pool/base_mermaid.py
@@ -193,7 +193,7 @@ class MermaidBase(ModelBase):
         saving_original_sz_path = os.path.join(self.record_path, 'original_sz')
         os.makedirs(saving_original_sz_path, exist_ok=True)
         fname_list = list(self.fname_list)
-        ires.save_transfrom(new_phi, self.spacing, saving_original_sz_path, fname_list)
+        ires.save_transfrom(new_phi, new_spacing, saving_original_sz_path, fname_list)
         reference_list = pair_path[0]
         fname_list = [fname + '_warped' for fname in self.fname_list]
         ires.save_image_with_given_reference(warped, reference_list, saving_original_sz_path, fname_list)
@@ -204,9 +204,9 @@ class MermaidBase(ModelBase):
         ires.save_image_with_given_reference(None, reference_list, saving_original_sz_path, fname_list)
         if inverse_phi is not None:
             inverse_phi = (inverse_phi + 1) / 2. if not use_01 else inverse_phi
-            new_inv_phi, inv_warped, _ = ires.resample_warped_phi_and_image(target_list, inverse_phi, spacing)
+            new_inv_phi, inv_warped, new_spacing = ires.resample_warped_phi_and_image(target_list, inverse_phi, spacing)
             fname_list = [fname + '_inv' for fname in self.fname_list]
-            ires.save_transfrom(new_inv_phi, self.spacing, saving_original_sz_path, fname_list)
+            ires.save_transfrom(new_inv_phi, new_spacing, saving_original_sz_path, fname_list)
             fname_list = [fname + '_inv_warped' for fname in self.fname_list]
             ires.save_image_with_given_reference(inv_warped, reference_list, saving_original_sz_path, fname_list)
 
